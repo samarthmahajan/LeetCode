a9cd6375babf44e604af98b52b9a742d03ae46cc
samarthmahajan <109908227+samarthmahajan@users.noreply.github.com>
2024-09-11 23:22:24 +0530

    Update 2220-minimum-bit-flips-to-convert-number.java

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
 
-            int xorRes = start ^ goal; // what this do is convert and create the xor result by thaat what i mean is all the bits which are different are as one
+            int xorRes = start ^ goal; // what this do is convert and create the xor result, that means  all the bits which are different are represented as 1.
         return Integer.bitCount(xorRes);
     }
 }
