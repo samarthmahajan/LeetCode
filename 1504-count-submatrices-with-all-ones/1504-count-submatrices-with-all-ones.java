f67fc12e7e0dc7b638c01a0cc23f6bc50d2f337c
samarthmahajan <109908227+samarthmahajan@users.noreply.github.com>
2025-08-21 13:15:19 +0530

    Time: 10 ms (33.9%), Space: 45.6 MB (47.26%) - LeetHub

--- 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.java
+++ 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.java
@@ -7,12 +7,10 @@ class Solution {
         int ans = 0;
 
         for (int r = 0; r < m; r++) {
-            // 1) Build histogram for this row
             for (int c = 0; c < n; c++) {
                 heights[c] = (mat[r][c] == 1) ? heights[c] + 1 : 0;
             }
 
-            // 2) Monotonic increasing stack of (height, count)
             Deque<int[]> st = new ArrayDeque<>();
             long sum = 0;
 
@@ -22,15 +20,14 @@ class Solution {
 
                 while (!st.isEmpty() && st.peek()[0] >= h) {
                     int[] top = st.pop();
-                    sum -= (long) top[0] * top[1]; // remove prior contribution
-                    cnt += top[1];                 // merge segments
+                    sum -= (long) top[0] * top[1]; 
+                    cnt += top[1];               
                 }
 
                 if (h > 0) {
                     sum += (long) h * cnt;
                     st.push(new int[]{h, cnt});
                 } else {
-                    // h == 0 breaks all rectangles ending here
                     sum = 0;
                     st.clear();
                 }
